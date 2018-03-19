31d44ca8e031ab68805cf88beb758ed92da2e4d6
Robin Cher <robin_cher@ida-gds.com>
2018-03-19 11:54:28 +0800

    Improve some test specs

--- spec/scripts/IControlUtilSpec.js
+++ spec/scripts/IControlUtilSpec.js
@@ -3,36 +3,31 @@ const sinon = require('sinon');
 const IControlUtil = require('../../lib/IControlUtil');
 const IControlUtilSpecStub = require('../stub/IControlUtilSpecStub.json');
 
+//Create base test IControl Instance for error
+let IControlClient = new IControlUtil({
+    host: '127.0.0.1',
+    ca: 'Some Fake Contents',
+    token: '1234', //Authorization Token for Basic Auth
+    output: true //Set to true if you want to see output
+});
+
+let testPath = '/mgmt/tm/net/bwc/policy';
+let testBody = {
+    key : "value"
+}
+
 describe('iControl REST API Client Test Spec - Normal Cases',  () => {
-
-    //Create base test IControl Instance
-    let IControlClient = new IControlUtil({
-        host: '127.0.0.1',
-        ca: 'Some Fake Contents',
-        token: 'SomeCA', //Authorization Token for Basic Auth
-        output: true //Set to true if you want to see output
-    });
-
-    let testPath = '/mgmt/tm/net/bwc/policy';
-
-    let testBody = {
-        key : "value"
-    }
-
     describe('List / Get  F5 iControl Collection',  () => {
-
         let listCollectionStub = sinon.stub(IControlClient, 'list');
         listCollectionStub.withArgs(testPath).resolves(IControlUtilSpecStub.ListCollectionStubResponse);
 
         it('Get F5 iControl Collection Normal Case', (done) => {
-
             IControlClient.list(testPath).then((res) => {
                 expect(listCollectionStub.called).to.be.true;
                 expect(res).to.be.eql(IControlUtilSpecStub.ListCollectionStubResponse);
                 done();
             })
             .catch(done);
-
         });
 
         after(() => {
@@ -47,14 +42,12 @@ describe('iControl REST API Client Test Spec - Normal Cases',  () => {
         createCollectionStub.withArgs(testPath,testBody).resolves(IControlUtilSpecStub.CollectionStubResponse);
 
         it('Create F5 iControl Collection Normal Case', (done) => {
-
             IControlClient.create(testPath,testBody).then((res) => {
                 expect(createCollectionStub.called).to.be.true;
                 expect(res).to.be.eql(IControlUtilSpecStub.CollectionStubResponse);
                 done();
             })
             .catch(done);
-
         });
 
         after(() => {
@@ -67,7 +60,6 @@ describe('iControl REST API Client Test Spec - Normal Cases',  () => {
         updateCollectionStub.withArgs(testPath,testBody).resolves(IControlUtilSpecStub.CollectionStubResponse);
 
         it('Update F5 iControl Collection Normal Case', (done) => {
-
             IControlClient.update(testPath,testBody).then((res) => {
                 expect(updateCollectionStub.called).to.be.true;
                 expect(res).to.be.eql(IControlUtilSpecStub.CollectionStubResponse);
@@ -82,13 +74,11 @@ describe('iControl REST API Client Test Spec - Normal Cases',  () => {
         });
     });
 
-
     describe('Delete a F5 iControl Collection',  () => {
         let deleteCollectionStub = sinon.stub(IControlClient, 'delete');
         deleteCollectionStub.withArgs(testPath).resolves(undefined);
 
         it('Update F5 iControl Collection Normal Case', (done) => {
-
             IControlClient.delete(testPath).then((res) => {
                 expect(res).to.be.undefined;
                 done();
@@ -99,5 +89,70 @@ describe('iControl REST API Client Test Spec - Normal Cases',  () => {
         after(() => {
             deleteCollectionStub.restore();
         });
+    });
+});
+
+describe('iControl REST API Client Test Spec - Error Cases',  () => {
+
+    describe('iControl REST API Client Test Spec - Error Cases : No Request Body',  () => {
+
+        it('Create F5 iControl Collection without a request body', (done) => {
+            IControlClient.create(testPath, undefined).then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoCreateReqBodyRes);
+                done();
+            });
+        });
+
+        it('Update F5 iControl Collection without a request body', (done) => {
+            IControlClient.update(testPath, undefined).then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoUpdateReqBodyRes);
+                done();
+            });
+        });
+
+    });
+
+    describe('iControl REST API Client Test Spec - Error Cases : No API Path',  () => {
+
+        it('Get F5 iControl Collection without a path', (done) => {
+            IControlClient.list().then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoListPathRes);
+                done();
+            });
+        });
+
+        it('Create F5 iControl Collection without a path', (done) => {
+            IControlClient.create(undefined, testBody).then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoCreatePathRes);
+                done();
+            });
+        });
+
+        it('Update  F5 iControl Collection without a path', (done) => {
+            IControlClient.update(undefined, testBody).then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoUpdatePathRes);
+                done();
+            });
+        });
+
+        it('Delete F5 iControl Collection without a path', (done) => {
+            IControlClient.delete().then((res) => {
+                expect(res).to.be.undefined;
+            }).catch((err) => {
+                expect(err.message).to.equal(IControlUtilSpecStub.ErrorStubResponse.NoDeletePathRes);
+                done();
+            });
+        });
+
     });
 });
