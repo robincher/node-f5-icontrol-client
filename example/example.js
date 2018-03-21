58aa77f4b25ad64be20b0a4338d4a29c4f86e89a
Robin Cher <robincher@gmail.com>
2018-03-21 15:14:22 +0800

    Fix missing syntax

--- example/example.js
+++ example/example.js
@@ -5,15 +5,15 @@ const fs = require('fs');
 
 let iControlClient = new IControlUtil({
     host: config.test.host,
-    ca: fs.readFileSync(config.test.ca, {encoding: 'utf-8'),
+    ca: fs.readFileSync(config.test.ca, {encoding: 'utf-8'}),
     token: config.test.token, //Authorization Token for Basic Auth
     output: true //Set to true if you want to see output
 });
 
-iControlClient.list('/mgmt/tm/net/bwc/policy').then(function(res)  {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+iControlClient.list('/mgmt/tm/net/bwc/policy').then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+})
 
